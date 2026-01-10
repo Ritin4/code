71e17c79d87dc9ace76b697ba94ea5bbf295874a
Ritin4 <ritin.k.4500@gmail.com>
2026-01-10 22:57:44 +0530

    two freq ele in arr revison

--- Accumulator Variables/two_frequent_elements.py
+++ Accumulator Variables/two_frequent_elements.py
@@ -1,45 +1,40 @@
 #Find the two elements that appear the most number of times in an array.
 
 def two_frequent_elements(arr):
-    # Count frequencies
-    frequency = {}
+    frequency_list = {}
 
-    for x in arr:
-        if x not in frequency:
-            frequency[x] = 1
+    for ele in arr:
+        if ele not in frequency_list:
+            frequency_list[ele] = 1
         else:
-            frequency[x] += 1
+            frequency_list[ele] += 1
 
-    # Track top two frequencies
-    first_element = None
-    second_element = None
-    first_count = 0
-    second_count = 0
+    first_ele = None
+    second_ele = None
+    first_max = 0
+    second_max = 0
 
-    # Evaluate each element's frequency
-    for element, count in frequency.items():
-        if count > first_count:
-            # Shift current first into second
-            second_element = first_element
-            second_count = first_count
+    for ele, count in frequency_list.items():
+        if count > first_max:
+            second_ele = first_ele
+            second_max = first_max
 
-            # Update first
-            first_element = element
-            first_count = count
+            first_ele = ele
+            first_max = count
 
-        elif count > second_count:
-            second_element = element
-            second_count = count
-
-    print("first_element", first_element)
-    print("second_element", second_element)
-
-
-    return first_element, second_element
+        elif count > second_max:
+             second_ele = ele
+             second_max = count 
+    
+    print("freq_list: ", frequency_list)
+    print("first ele: ", first_ele)
+    print("first_max: ", first_max)
+    print("second_ele: ", second_ele)
+    print("second_max: ", second_max)
     
 
 # two_frequent_elements([1,2,1,2,2,3,4,4,5])
-two_frequent_elements([1, 1, 1, 1, 5])
+two_frequent_elements([1, 1, 1, 1, 4])
 
 
 
