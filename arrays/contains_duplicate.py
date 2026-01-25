acf19a591a86ec9d1da3d72390ea48161a2a93e0
Ritin4 <ritin.k.4500@gmail.com>
2026-01-25 22:38:46 +0530

    leetcode 201

--- arrays/contains_duplicate.py
+++ arrays/contains_duplicate.py
@@ -1,6 +1,32 @@
-def containsDuplicate(self, nums):
-        nums.sort()
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
-                return True
+'''
+Problem: Given an integer array, return true if any of the element is present at least twice and false if all the elements are distinct
+Input: array of integers 
+Output: True if there are duplicates, False if all are distinct
+'''
+
+def contains_duplicate(arr):
+    if not arr:
         return False
+
+    tracker = {}
+    
+    for num in arr:
+        if not isinstance(num, int):
+            raise ValueError("Not a valid integer")
+
+        if num not in tracker.keys():
+            tracker[num] = 1
+        else:
+            tracker[num] += 1
+        
+        print(tracker)
+
+
+    for count in tracker.values():
+        if count > 1:
+            return True
+    
+    return False
+
+
+print(contains_duplicate([1,2,1,1,1,3,4,5]))
